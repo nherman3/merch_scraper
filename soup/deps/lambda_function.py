62d5021d8947f032e689cbe68cd839a51be7f3c6
nherman3 <129703616+nherman3@users.noreply.github.com>
2024-05-02 16:19:25 -0400

    second website added for scraping and notifications customized

--- soup/deps/lambda_function.py
+++ soup/deps/lambda_function.py
@@ -17,122 +17,128 @@ def remove_keywords(s, keywords):
     return s
 
 def lambda_handler(event,context):
-    for key in [event['file1'], event['file2']]:
-        s3 = boto3.resource('s3')
+    key = event['file']
 
-        if key[0:2] == 'AD':
-            product_show = 'American Dad!'
+    if key[0:2] == 'AD':
+        product_show = 'American Dad!'
+    else:
+        product_show = 'Family Guy'
+    strip_len = len(product_show)
+
+    s3 = boto3.resource('s3')
+
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(OUTPUT_TABLE)
+
+    # get object from s3 bucket
+    obj = s3.Object(BUCKET, key)
+
+    # read contents
+    file_content = obj.get()['Body'].read().decode('utf-8')
+
+    soup = BeautifulSoup(file_content, 'html.parser')
+
+    # Find all product divs within the section
+    product_divs = soup.select('section.product-grid div[class^="four columns"]')
+
+    # Iterate over each product div
+    for product in product_divs:
+        # Get product name
+        product_name_full = product.find('h3').text.strip()[strip_len:].strip()
+        product_name = remove_keywords(product_name_full,
+                                       ["hard enamel pin", "enamel pin", "pin le", "Tee",
+                                        "sticker", "Clear Die Cut", "(limited"])
+        product_name = re.sub(r'^\s*-\s*', '', product_name).strip()
+
+        # Get product category
+        if re.findall('pin', product_name_full, re.IGNORECASE):
+            product_type = 'Enamel Pin'
+        elif re.findall('tee', product_name_full, re.IGNORECASE):
+            product_type = 'T-Shirt'
+        elif re.findall('sticker', product_name_full, re.IGNORECASE):
+            product_type = 'Sticker'
         else:
-            product_show = 'Family Guy'
-        strip_len = len(product_show)
+            product_type = 'T-Shirt?'
+        # Get product price
+        product_price = product.find('h4').text.strip()
+        # second_index = product_price.find('$', product_price.find('$') + 1)
+        second_index = product_price.find('$', product_price.find('$') + 1)
+        if second_index != -1:
+            product_price = product_price[:second_index]
 
-        dynamodb = boto3.resource("dynamodb")
-        table = dynamodb.Table(OUTPUT_TABLE)
+        # Get Sold Out Status
+        if product.find('div'):
+            templist = [status.text.strip() for status in product.find_all('div')]
+            product_avail = ' - '.join(templist)
+        else:
+            product_avail = 'Available'
+        # <div class="sold-out">Sold Out</div>
 
-        # get object from s3 bucket
-        obj = s3.Object(BUCKET, key)
+        # Get product URL
+        product_url = 'https://www.toddland.com' + product.find('a')['href']
 
-        # read contents
-        file_content = obj.get()['Body'].read().decode('utf-8')
+        # Get product image URL
+        product_image = product.find('img')['src']
+        product_image = 'https:' + re.sub('_large', '', product_image)
+        product_id = re.sub('_large', '', product_image)[-10:]
 
-        soup = BeautifulSoup(file_content, 'html.parser')
+        # Check if the item exists
+        existing_item = table.get_item(Key={'id': product_id})
 
-        # Find all product divs within the section
-        product_divs = soup.select('section.product-grid div[class^="four columns"]')
-
-        # Iterate over each product div
-        for product in product_divs:
-            # Get product name
-            product_name_full = product.find('h3').text.strip()[strip_len:].strip()
-            product_name = remove_keywords(product_name_full,
-                                           ["hard enamel pin", "enamel pin", "pin le", "Tee",
-                                            "sticker", "Clear Die Cut", "(limited"])
-            product_name = re.sub(r'^\s*-\s*', '', product_name).strip()
-
-            # Get product category
-            if re.findall('pin', product_name_full, re.IGNORECASE):
-                product_type = 'Enamel Pin'
-            elif re.findall('tee', product_name_full, re.IGNORECASE):
-                product_type = 'T-Shirt'
-            elif re.findall('sticker', product_name_full, re.IGNORECASE):
-                product_type = 'Sticker'
-            else:
-                product_type = 'T-Shirt?'
-            # Get product price
-            product_price = product.find('h4').text.strip()
-            # second_index = product_price.find('$', product_price.find('$') + 1)
-            second_index = product_price.find('$', product_price.find('$') + 1)
-            if second_index != -1:
-                product_price = product_price[:second_index]
-
-            # Get Sold Out Status
-            if product.find('div'):
-                templist = [status.text.strip() for status in product.find_all('div')]
-                product_avail = ' - '.join(templist)
-            else:
-                product_avail = 'Available'
-            # product_sold_out.append(product_avail)
-            # <div class="sold-out">Sold Out</div>
-
-            # Get product URL
-            product_url = 'https://www.toddland.com' + product.find('a')['href']
-
-            # Get product image URL
-            product_image = product.find('img')['src']
-            product_image = 'https:' + re.sub('_large', '', product_image)
-            product_id = re.sub('_large', '', product_image)[-10:]
-
-            # Check if the item exists
-            existing_item = table.get_item(Key={'id': product_id})
-
-            if 'Item' in existing_item:
-                # If item exists, update it
-                update_response = table.update_item(
-                    Key={'id': product_id},
-                    UpdateExpression="set #P = :p, #A = :a",
-                    ExpressionAttributeNames={
-                        '#P': 'Price',
-                        '#A': 'Availability'  # Using an expression attribute name for special character handling
+        if 'Item' in existing_item:
+            # If item exists, update it
+            update_response = table.update_item(
+                Key={'id': product_id},
+                UpdateExpression="set #P = :p, #A = :a",
+                ExpressionAttributeNames={
+                    '#P': 'Price',
+                    '#A': 'Availability'  # Using an expression attribute name for special character handling
+                },
+                ExpressionAttributeValues={
+                    ':p': product_price,
+                    ':a': product_avail
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+        else:
+            # If the item does not exist, add it and send a notification
+            try:
+                response = table.put_item(
+                    Item={
+                        'id': product_id,
+                        '_Name': product_name,
+                        'Category': product_type,
+                        'Price': product_price,
+                        'Availability': product_avail,
+                        'URL': product_url,
+                        'Image': product_image,
+                        '_Show': product_show
                     },
-                    ExpressionAttributeValues={
-                        ':p': product_price,
-                        ':a': product_avail
-                    },
-                    ReturnValues="UPDATED_NEW"
+                    ConditionExpression='attribute_not_exists(id)'
                 )
-            else:
-                # If the item does not exist, add it and send a notification
-                try:
-                    response = table.put_item(
-                        Item={
-                            'id': product_id,
-                            '_Name': product_name,
-                            'Category': product_type,
-                            'Price': product_price,
-                            'Availability': product_avail,
-                            'URL': product_url,
-                            'Image': product_image,
-                            '_Show': product_show
-                        },
-                        ConditionExpression='attribute_not_exists(id)'
-                    )
-                    # Send notification for new item
-                    send_notification(f'New {product_type}: {product_name} has been posted\n\n{product_url} ')
+                # Send notification for new item depending on show or item type
 
-                except dynamodb.meta.client.exceptions.ConditionalCheckFailedException:
-                    # This exception means the item already exists
-                    # Normally this shouldn't occur due to the earlier check
-                    pass
+                if product_type == 'Enamel Pin':
+                    send_notification(f'Toddland {product_show} Inventory Notification',
+                                      f'New {product_type}: {product_name} has been posted\n\n{product_url} ')
+                elif product_show == 'American Dad!':
+                    send_notification(f'Toddland {product_show} Inventory Notification',
+                                      f'New {product_type}: {product_name} has been posted\n\n{product_url} ')
 
-        return {
-            'statusCode': 200,
-            'body': json.dumps('success')
-        }
+            except dynamodb.meta.client.exceptions.ConditionalCheckFailedException:
+                # This exception means the item already exists
+                # Normally this shouldn't occur due to the earlier check
+                pass
 
-def send_notification(message):
+    return {
+        'statusCode': 200,
+        'body': json.dumps('success')
+    }
+
+def send_notification(subject, message):
     sns = boto3.client('sns')
     sns.publish(
         TopicArn='arn:aws:sns:us-east-2:992382680174:Toddland_Update',
         Message=message,
-        Subject='Toddland American Dad Inventory Update Notification'
+        Subject=subject
     )
